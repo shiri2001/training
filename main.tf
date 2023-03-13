658d189356f85ea7fe0743c416648cbfc91ca1b2
shiri2001 <109620909+shiri2001@users.noreply.github.com>
2023-03-13 11:42:39 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -41,27 +41,27 @@ resource "aws_key_pair" "ssh_key" {
   public_key = tls_private_key.ssh_key.public_key_openssh
 }
 
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic"
+#  resource "aws_security_group" "allow_tls" {
+#    name        = "allow_tls"
+#    description = "Allow TLS inbound traffic"
+#
+#    ingress {
+#      description = "TLS from VPC"
+#      from_port   = 22
+#      to_port     = 22
+#      protocol    = "tcp"
+#      cidr_blocks = ["0.0.0.0/0"]
+#    }
 
-  ingress {
-    description = "TLS from VPC"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#    egress {
+#      from_port   = 0
+#      to_port     = 0
+#      protocol    = "-1"
+#      cidr_blocks = ["0.0.0.0/0"]
+#    }
+#  }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_instance" "web" {
+resource "aws_instance" "vm" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = var.my_instance_type
   key_name               = aws_key_pair.ssh_key.key_name
