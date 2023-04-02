eb32345d9cce871703285b06bc9e5e78c11c2b2e
shiri2001 <Shiri@shirile01@gmail.com>
2023-04-02 18:03:50 +0300

    edited bash script

--- mount.sh
+++ mount.sh
@@ -1,3 +1,31 @@
+#!/bin/bash
+
+# This script's purpose is to mount EBS volume to VM.
+
+MOUNTDIR="/apps/volume/new-vol"
+
+# Checks whether volume is already mounted.
+
+if grep -qs ${MOUNTDIR} /proc/mounts; then
+    echo "Volume already mounted."
+else
+    echo "Volume not mounted yet. mounting...."
+fi
+
+# Mounting volume
 mkfs -t xfs /dev/xvdf
-mkdir -p /apps/volume/new-vol
-mount /dev/sdh /apps/volume/new-vol
+mkdir -p ${MOUNTDIR}
+mount /dev/sdh ${MOUNTDIR}
+
+# Check if mounted successfully.
+if [ $? -eq 0 ]; then
+    echo "Volume mounted."
+else
+    echo "Mounting failed."
+fi
+
+# Error handling
+err() {
+  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&2
+}
+
