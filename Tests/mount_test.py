55936c22d453adc741030327461e739f783b9ddf
shiri2001 <Shiri@shirile01@gmail.com>
2023-04-17 17:09:40 +0300

    fixed various issues

--- Tests/mount_test.py
+++ Tests/mount_test.py
@@ -2,13 +2,13 @@ import os
 import boto3
 
 ec2 = boto3.resource('ec2')
-os.system('cmd /c "terraform output -raw volume_id > volume_id.txt"')
-infile = open('volume_id.txt','r')
-volume_id = infile.read()
+os.system('cmd /c "cd .. & cd Infrastructure & terraform output -raw app_volume_id > app_volume_id.txt"')
+infile = open('app_volume_id.txt','r')
+app_volume_id = infile.read()
 infile.close()
-if volume_id == "":
+if app_volume_id == "":
     print("No volume created")
 else:
-    volume = ec2.Volume(volume_id)
+    volume = ec2.Volume(app_volume_id)
     volume_state = volume.state
-    print("The state of volume {} is {}".format(volume_id,volume_state))
+    print("The state of volume {} is {}".format(app_volume_id,volume_state))
