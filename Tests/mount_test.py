2ce28d09b844dc6f8ea0bc399e2ec9fe5bb42248
shiri2001 <Shiri@shirile01@gmail.com>
2023-04-30 18:19:57 +0300

    fixed file nonexistant error in mount test

--- Tests/mount_test.py
+++ Tests/mount_test.py
@@ -5,12 +5,13 @@ ec2 = boto3.resource('ec2')
 os.system(
     'cmd /c "cd .. & cd Infrastructure & \
         terraform output -raw app_volume_id > app_volume_id.txt"')
-infile = open('app_volume_id.txt', 'r')
-app_volume_id = infile.read()
-infile.close()
-if app_volume_id == "":
-    print("No volume created")
-else:
+
+if os.path.isfile('.//app_volume_id.txt') == True:
+    infile = open('app_volume_id.txt', 'r')
+    app_volume_id = infile.read()
+    infile.close()
     volume = ec2.Volume(app_volume_id)
     volume_state = volume.state
     print("The state of volume {} is {}".format(app_volume_id, volume_state))
+else:
+    print("volume creation failed")
