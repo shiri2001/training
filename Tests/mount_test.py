a55387bed40610427b4e4801f2013b253a083744
shiri2001 <Shiri@shirile01@gmail.com>
2023-05-01 12:51:26 +0300

    fixed linter issues

--- Tests/mount_test.py
+++ Tests/mount_test.py
@@ -5,6 +5,8 @@ ec2 = boto3.resource('ec2')
 os.system(
     'cmd /c "cd .. & cd Infrastructure & \
         terraform output -raw app_volume_id > app_volume_id.txt"')
+
+
 def test_mount():
     if os.path.isfile('.//app_volume_id.txt') is True:
         infile = open('app_volume_id.txt', 'r')
@@ -12,6 +14,7 @@ def test_mount():
         infile.close()
         volume = ec2.Volume(app_volume_id)
         volume_state = volume.state
-        print("The state of volume {} is {}".format(app_volume_id, volume_state))
+        print("The state of volume {} is {}".format(
+            app_volume_id, volume_state))
     else:
         print("volume creation failed")
