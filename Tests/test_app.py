1d873971cfb5066598bac91881bc3ded6da327fc
shiri2001 <Shiri@shirile01@gmail.com>
2023-05-15 13:36:21 +0300

    minor text linter fix

--- Tests/test_app.py
+++ Tests/test_app.py
@@ -15,7 +15,9 @@ from App import app  # noqa: E402
 
 class TestApp:
     def test_print_output(self):
-        assert app.game("ROCK") == (
-            "Computer chose PAPER! too bad! you lost!" or
-            "Computer chose SCISSORS! You won!" or
-            "Computer chose ROCK! It's a draw!")
+        assert app.game("ROCK") == \
+            "Computer chose PAPER! too bad! you lost!"\
+            or \
+            "Computer chose SCISSORS! You won!"\
+            or \
+            "Computer chose ROCK! It's a draw!"
