f43c54018b07b1a2bc09a669f50c57210e576464
shiri2001 <Shiri@shirile01@gmail.com>
2023-05-24 20:56:00 +0300

    (MINOR) test has changed to fit the new app

--- Tests/test_app.py
+++ Tests/test_app.py
@@ -5,24 +5,15 @@ Tests if the app is running correctly
 import subprocess
 import sys
 import os
-from flask import render_template
-import random
+import pytest
 PATH = str(subprocess.check_output(
         "cd", shell=True, universal_newlines=True)).rstrip()
 option_list = ["rock", "paper", "scissors"]
 
 sys.path.append(os.path.abspath(PATH))
 
-from App import app  # noqa: E402
 
-
-class TestApp:
-    def test_print_output(self):
-        computer_choice = random.choice(option_list)
-        assert app.rock() == render_template(
-            "rock.html", cpu_choice=computer_choice,
-            result="It's a draw!") or\
-            render_template("rock.html", cpu_choice=computer_choice,
-                            result="You lost!") or\
-            render_template("rock.html", cpu_choice=computer_choice,
-                            result="You won!")
+@pytest.fixture
+def test_title(client):
+    response = client.get("/")
+    assert b"<title>Rock Paper scissors!</title>" in response.data
